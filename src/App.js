aa15df950d842900747c12cb1ff9a47a1fdbe462
Cottlete <shikulik@gmail.com>
2024-06-25 00:15:28 +0300

    image add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 function App() {
   return (
     <div className="wrapper">
-      hellod
+    <header>
+      <div className="headerLeft">
+      <img width={40} height={40} src="/img/logo.png" />
+      <div className="headerInfo">
+        <h3>React Sneakers</h3>
+        <p>Магазин лучших кроссовок</p>
+      </div>
+      </div>
+      <ul className="headerRight">
+        <li>
+        <img width={18} height={18} src="/img/cart.svg" />
+          <span>1205 руб.</span>
+        </li>
+        <li>
+        <img width={18} height={18} src="/img/user.svg" />
+        </li>
+
+      </ul>
+    </header>
+    <div className="content">
+      <h1>Все кроссовки</h1>
+      ........
+    </div>
     </div>
   );
 }
